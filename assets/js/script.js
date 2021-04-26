64d5021d97ef121d324c74883defc0503ea256c2
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-04-26 14:42:31 -0700

    added cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var cityNameEl = document.getElementById("city");
 var cityInfo = function() {
     // grap weather info from url query string
     var cityName = cityNameEl.value;
-   
+
     if (cityName) {
         // display city name on the page
         // document.getElementById("city-name").innerHTML=cityName;        
@@ -32,17 +32,71 @@ var weatherInfo = function(lat,lon) {
                 var output = "";
                 output+="<h1>"+ cityName+ "</h1>";
                 output+="<p>Current Temperature: " + data.current.temp+"&deg;F</p>";
-                output+="<p>Wind Speed: " + data.current.speed+" MPH</p>";
-                output+="<p>Humidity: " + data.current.humidity+"</p>";
-                output+="<p>UV Index: " + data.current.uvi +"</p>";
+                output+="<p>Wind Speed: " + data.current.wind_speed+" MPH</p>";
+                output+="<p>Humidity: " + data.current.humidity+"%</p>";
+                output+="<p>UV Index: <span id='uvi'>" + data.current.uvi +"</span></p>";
+                //ADDS TO INDEX.HTML 
                 document.getElementById("current").innerHTML=output;
-                //forcast 
+
+                //uvi index background color 
+                if (data.current.uvi < 3) {
+                    //green 
+                    document.getElementById("uvi").className="btn-success";
+                }else if (data.current.uvi > 3 && data.current.uvi < 6 ) {
+                    //yellow 
+                    document.getElementById("uvi").className="btn-warning";
+                }else {
+                    //red 
+                    document.getElementById("uvi").className="btn-danger";
+                }
+            
+                //forecast 
+                var forceastDivEle = document.getElementById("forecast");
+                //clear exisiting data 
+                forceastDivEle.innerHTML="";
+
                 if( data.daily.length > 0 ){
                     for(var i =0; i < 5; i++){
-                        //Create a card 
+                        //Create a card div 
+                        var cardDiv = document.createElement("div");
+                        //adding css to the div 
+                        cardDiv.setAttribute('class','card col-2 m-1 p-1'); 
+                        
+                        //date 
+                        var cardTitleEle = document.createElement("h5");
+                        cardTitleEle.setAttribute('class', 'card-title'); 
+                        //converting UNIX timestamo to DATE 
+                        var forecastDate = new Date(data.daily[i].dt  * 1000).toLocaleDateString("en-US");
+                        console.log(forecastDate)
+                        cardTitleEle.textContent=forecastDate;
+
+                        //temperature 
+                        var cardBodyEle = document.createElement("p");
+                        cardBodyEle.setAttribute('class', 'card-text'); 
+                        cardBodyEle.textContent=" Temp: " + data.daily[i].temp.max+"°F";
+
+                         //wind
+                        var windEle = document.createElement("p");
+                        windEle.setAttribute('class', 'card-text'); 
+                        windEle.textContent=" Wind: " + data.daily[i].wind_speed+" MPH";
+
+                        //humidity
+                        var humidityEle = document.createElement("p");
+                        humidityEle.setAttribute('class', 'card-text'); 
+                        humidityEle.textContent=" Humidity: " + data.daily[i].humidity+"%"; 
+
+
+                        console.log(cardDiv);
+                        console.log(cardTitleEle);
+                        //Appending all information to my DIV CARD 
+                        cardDiv.append(cardTitleEle,cardBodyEle,windEle,humidityEle); 
+                        console.log("updated card", cardDiv);
                         //add allthe information to it 
                         //then append the div forecast 
                         console.log("forecast value ", data.daily[i]); 
+
+                        //display on the HTML Page 
+                        forceastDivEle.append(cardDiv)
                     }
                 }
                 
